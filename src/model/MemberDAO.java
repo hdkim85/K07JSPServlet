fc37fb565cfd34fb61ecd6b4579afba63ee45b3c
hdkim85 <hdkim85@gmail.com>
2020-05-04 16:15:16 +0900

    0504

--- src/model/MemberDAO.java
+++ src/model/MemberDAO.java
@@ -4,6 +4,8 @@ import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class MemberDAO {
@@ -26,7 +28,7 @@ public class MemberDAO {
 			e.printStackTrace();
 		}
 	}
-	
+	//방법1 : 회원의 존재유무만 판단한다.
 	public boolean isMember(String id, String pass) {
 		
 		String sql = "SELECT COUNT(*) FROM member WHERE id=? AND pass=?";
@@ -59,5 +61,123 @@ public class MemberDAO {
 		}
 		return isFlag;
 	}
+	
+	
+	
+	//방법2 : 회원인증 후 MemberDTO객체로 회원정보를 반환한다.
+	public MemberDTO getMemberDTO(String uid, String upass) {
+		//DTO객체를 생성한다.
+		MemberDTO dto = new MemberDTO();
+		//쿼리문을 작성		
+		String query = "SELECT id, pass, name FROM "
+				+ " member WHERE id=? AND pass=?";
+		
+		try {
+			//prepared 객체 생성
+			psmt = con.prepareStatement(query);
+			//쿼리문의 인파라미터 설정
+			psmt.setString(1, uid);
+			psmt.setString(2, upass);
+			//오라클로 쿼리문 전송 및 결과셋(ResultSet) 반환
+			rs = psmt.executeQuery();
+			//오라클이 반환해준 ResultSet이 있는지 확인
+			if(rs.next()) {
+				//true를 반환했다면 결과셋 있음
+				//DTO객체에 회원 레코드의 값을 저장한다.
+				dto.setId(rs.getString("id"));
+				dto.setPass(rs.getString("pass"));
+				dto.setName(rs.getString(3));
+			}
+			else {
+				//false를 반환했다면 결과셋 없음
+				System.out.println("결과셋이 없습니다.");
+				
+			}
+			
+		} catch (Exception e) {
+			System.out.println("getMemberDTO오류");
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		
+		//내가 작성한 내용
+		/*String sql = "SELECT * FROM member WHERE id=? AND pass=?";
+		
+		
+		try {
+			//prepare 객체로 쿼리문 전송
+			psmt = con.prepareStatement(sql);
+			//인파라미터 설정
+			psmt.setString(1, uid);			
+			psmt.setString(2, upass);
+			//쿼리실행
+			rs = psmt.executeQuery();
+			//실행결과를 가져오기 위해 next()호출			
+			
+			
+			while(rs.next()) {
+				dto.setId(rs.getString(1));
+				dto.setPass(rs.getString(2));
+				dto.setName(rs.getString(3));
+				dto.setRegidate(rs.getDate(4));
+				
+			}			
+			
+		} catch (Exception e) {		
+			e.printStackTrace();
+		}*/
+		
+		
+		//DTO객체를 반환한다.
+		return dto;
+		
+	}
+	
+	public Map<String, String> getMemberMap(String id, String pwd){
+		
+		Map<String, String> maps = new HashMap<String, String>();
+		
+		String query = "SELECT id, pass, name FROM "
+				+ " member WHERE id=? AND pass=?";
+		
+		try {
+			//prepared 객체 생성
+			psmt = con.prepareStatement(query);
+			//쿼리문의 인파라미터 설정
+			psmt.setString(1, id);
+			psmt.setString(2, pwd);
+			//오라클로 쿼리문 전송 및 결과셋(ResultSet) 반환
+			rs = psmt.executeQuery();
+			//오라클이 반환해준 ResultSet이 있는지 확인
+			if(rs.next()) {
+				//true를 반환했다면 결과셋 있음
+				//DTO객체에 회원 레코드의 값을 저장한다.
+				maps.put("id", rs.getString(1));
+				maps.put("pass", rs.getString("pass"));
+				maps.put("name", rs.getString("name"));
+			}
+			else {
+				//false를 반환했다면 결과셋 없음
+				System.out.println("결과셋이 없습니다.");
+				
+			}
+			
+		} catch (Exception e) {
+			System.out.println("getMemberDTO오류");
+			e.printStackTrace();
+		}
+		
+		return maps;		
+	}
+	
+	
+	
+	
+	
+	
+	
 	
 }
